77a8b72ada32abae9eb99b0a5f8633ccbb36b7d8
Ludovic Herisson <ludovic_herisson@yahoo.fr>
2022-02-05 00:11:54 +0100

    Maj Accueil

--- footer.php
+++ footer.php
@@ -3,9 +3,9 @@
 ?>
     </main>
     <footer>
-        <article>
-        <p><a href="https://github.com/HerLudo/Space-Jam" target="_blank"id="git">Ludovic Herisson</a></p>
-        </article>
+        <!-- <div class="git"> -->
+            <a href="https://github.com/HerLudo/Space-Jam" target="_blank"><img src="pictures\GitHub-Mark-Light-64px.png" alt="Github logo" class="gitimg"></a></div>
+        <p>Ludovic Herisson</p>
     </footer>   
     </body>
 </html>
